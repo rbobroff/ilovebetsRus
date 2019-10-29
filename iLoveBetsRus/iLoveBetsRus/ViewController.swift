451b5561cc9bd18bbc3096103f72c76198e2296a
Roman Bobrov <r.bobroff@gmail.com>
2019-10-29 20:53:18 +0300

    Создан проект приложения

---
+++ iLoveBetsRus/iLoveBetsRus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iLoveBetsRus
+//
+//  Created by Roman Bobrov on 29.10.2019.
+//  Copyright © 2019 Roman Bobrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
