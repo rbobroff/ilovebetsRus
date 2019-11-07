0988d79e9751c8c0e87fa81b14106b7ded510f03
Roman Bobrov <r.bobroff@gmail.com>
2019-11-07 20:25:56 +0300

    Оптимизация приложения по 6-му уроку Школа iOS Разработчика. Для функции загрузки и распарсивания писали условия, что если не приходят данные или приходят в неправильном формате, выходить из функций (с помощью return)

--- iLoveBetsRus/iLoveBetsRus/Model/Model.swift
+++ iLoveBetsRus/iLoveBetsRus/Model/Model.swift
@@ -13,9 +13,19 @@ var articles: [Article] = []
 
 //
 
+var urlToData: URL {
+    //эта команда вернет путь в директорию пользователя. это массив всех папок, которые там есть. наши новости будут загружены в файл data.json в файловой системе устройства
+let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
+let urlPath = URL(fileURLWithPath: path)
+return urlPath
+}
+    
 //Функция загрузки новостей
 func loadNews() {
-    let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-06&sortBy=publishedAt&apiKey=239de8b6b6ed404ebcb61b80dfcf7bbc")
+  /*  let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-06&sortBy=publishedAt&apiKey=239de8b6b6ed404ebcb61b80dfcf7bbc") */
+    
+    let url = URL(string: "https://ilovebets.ru/mynewsapi2019/")
+    
     let session = URLSession(configuration: .default)
     
     //создаем объект. то есть, скачиваем информацию по ссылке и записываем в файл, который будет храниться в файловой системе устройства
@@ -23,21 +33,16 @@ func loadNews() {
         
         //проверяем, что если наш файл не равен "нулю", то значит файл загружен
         if urlFile != nil {
-        //эта команда вернет путь в директорию пользователя. это массив всех папок, которые там есть. наши новости будут загружены в файл data.json в файловой системе устройства
-        let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
-            let urlPath = URL(fileURLWithPath: path)
-            try? FileManager.default.copyItem(at: urlFile!, to: urlPath)
+        //эта команда вернет путь в директорию пользователя. это массив всех папок, которые там есть. наши новости будут загружены в файл data.json в файловой системе устройства. при оптимизации скопировали в выше "var urlToData: URL {
+        
+            try? FileManager.default.copyItem(at: urlFile!, to: urlToData)
             //Тестируем, что все ок. Печатаем в консоле путь к файлу при запуске приложения
-            print(urlPath)
-            
+            print(urlToData)
             //вызываем функцию parseNews после того, как сохранили наш файл:
             parseNews()
             //чтобы протестировать, печатаем количество новостей в массиве:
             print(articles.count)
-            
-    
         }
-        
     }
     
     //запускаем загрузку
@@ -46,24 +51,47 @@ func loadNews() {
 
 //Функция для распарсивания файла. распарсиваем файл data.json из файловой системы с данными
 func parseNews() {
-    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]+"/data.json"
-    let urlPath = URL(fileURLWithPath: path)
-    
     //получаем бинарные данные из нашего файла
-    let data = try? Data(contentsOf: urlPath)
-    //получаем рутовый словарь
-    let rootDictionary = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
+    let data = try? Data(contentsOf: urlToData)
+    //если data = 0, то выходим из функции func parseNews(). Сделано, чтобы приложение не вылетало, если с данными в файле что-то не так
+    if data == nil {
+        return
+    }
+    
+    let rootDictionaryAny = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+    //если данные в словаре = 0, то выходим из функции func parseNews()
+    if rootDictionaryAny == nil {
+        return
+    }
+    /*
+    //или можно сделать do catch проверку, чтобы проверить, если с данными в словаре не все ок, чтобы приложение не вылетало. при ошибке идем в блок catch
+    do {
+        //получаем рутовый словарь. если ошибка, идем в блок catch
+          let rootDictionary = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+    } catch {
+        //здесь можно показать пользователю, почему ошибка
+        //error.localizedDescription
+    }
+  */
+    //делаем преобразование типов
+    let rootDictionary = rootDictionaryAny as? Dictionary<String, Any>
+    //если данные в словаре = 0, то выходим из функции
+    if rootDictionary == nil {
+        return
+    }
     
     //получаем МАССИВ по ключу articles из статьи. Словарь состоит из [ключ, значение]
-    let array = rootDictionary!["articles"] as! [Dictionary<String, Any>]
-    
-    var returnArray: [Article] = []
-    // обходим массив. идем по объектам словаря
-    for dict in array {
-        let newArticle = Article(dictionary: dict)
-        //добавляем в цикле новые элементы
-        returnArray.append(newArticle)
+    if let array = rootDictionary!["articles"] as? [Dictionary<String, Any>] {
+        var returnArray: [Article] = []
+        // обходим массив. идем по объектам словаря
+        for dict in array {
+            let newArticle = Article(dictionary: dict)
+            //добавляем в цикле новые элементы
+            returnArray.append(newArticle)
     }
+    
     //присваиваем значению articles значения нашего словаря
     articles = returnArray
+}
+    
 }
