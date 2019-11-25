c5a16d448ff154352cbf1ff2f683f01ad54bbe9e
Roman Bobrov <r.bobroff@gmail.com>
2019-11-25 21:06:49 +0300

    1. Добавлен pod file 2. Добавлена Firebase SDK

--- iLoveBetsRus/iLoveBetsRus/AppDelegate.swift
+++ iLoveBetsRus/iLoveBetsRus/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase //при настройке push-уведомления
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure() //при настройке push-уведомления
+        
         
         //загружаем новости при загрузке приложения. использовали на начальных парах, чтоюы проверить, что новости загружаются и записываются в файл
         //loadNews()
