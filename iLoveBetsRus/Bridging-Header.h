8fc321aa41944c8a8e93d1d7d976c0eb510c4e0e
Roman Bobrov <r.bobroff@gmail.com>
2019-12-07 22:46:09 +0300

    Добавлен Bridging-Header.h

---
+++ iLoveBetsRus/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  iLoveBetsRus
+//
+//  Created by Roman Bobrov on 07.12.2019.
+//  Copyright © 2019 Roman Bobrov. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+
+#import <JSQMessagesViewController/JSQMessages.h>
