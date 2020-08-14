52658d4c7ae73a9db4fe96934c3aac78bde2845b
Roman Bobrov <r.bobroff@gmail.com>
2020-08-14 19:54:31 +0300

    Добавлена библиотека ApphudSDK в podfile и изменена минимальная версия iOS 11.2

---
+++ iLoveBetsRus/Pods/ApphudSDK/ApphudSDK/ApphudUtils.swift
@@ -0,0 +1,29 @@
+//
+//  ApphudUtils.swift
+//  subscriptionstest
+//
+//  Created by ren6 on 12/07/2019.
+//  Copyright © 2019 apphud. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This class will contain some utils, more will be added in the future.
+ */
+public class ApphudUtils: NSObject {
+
+    /**
+     Disables console logging.
+    */
+    @objc public class func enableDebugLogs() {
+        shared.isLoggingEnabled = true
+    }
+
+    internal static let shared = ApphudUtils()
+    private(set) var isLoggingEnabled = false
+
+    internal var optOutOfIDFACollection = false
+
+    internal var finishTransactions = false
+}
--- iLoveBetsRus/Pods/Target Support Files/Pods-iLoveBetsRus/Pods-iLoveBetsRus-frameworks.sh
+++ iLoveBetsRus/Pods/Target Support Files/Pods-iLoveBetsRus/Pods-iLoveBetsRus-frameworks.sh
@@ -161,6 +161,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ApphudSDK/ApphudSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/JSQMessagesViewController/JSQMessagesViewController.framework"
@@ -170,6 +171,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ApphudSDK/ApphudSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/JSQMessagesViewController/JSQMessagesViewController.framework"
